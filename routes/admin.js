a99029482353e7827af4535d097f90aa57ae37de
MEDAVRTX <86083191+medavrtx@users.noreply.github.com>
2022-10-21 22:11:48 -0700

    Add cart model

--- routes/admin.js
+++ routes/admin.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 // /admin => GET
-router.get('/admin', adminController.getAdmin);
+router.get('/', adminController.getAdmin);
 
 // /admin/products => GET
-router.get('/admin/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => GET
-router.get('/admin/edit-products', adminController.getEditProducts);
+router.get('/edit-products', adminController.getEditProducts);
 
 // /admin/add-product => GET
-router.get('/admin/add-product', adminController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/admin/add-product', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
