c6d717e31b80dbb5d930e562767541084e7f9823
MEDA <118144136+medaworld@users.noreply.github.com>
2022-10-31 17:33:59 -0700

    Retrieve paginated data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,13 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getHome = (req, res, next) => {
+  const page = req.query.page;
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then((products) => {
       res.render('shop/home', {
         products: products,
